ea37cdedefcfc1683d8f90c4fbf57b5d8bf903f6
rakesh0310 <rakesh03101999@gmail.com>
2019-12-21 11:48:23 +0530

    return added

--- randomWord.js
+++ randomWord.js
@@ -7,6 +7,6 @@ async function randomWord () {
     let random_word_url = host + '/words/randomWord?api_key=' + api_key;
     const response = await fetch(random_word_url);
     const json1 = await response.json();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-    console.log(json1.word);
+    return json1.word;
 }
 randomWord();
