533be4edd6ef6df6b254ab017244467928943c41
rakesh0310 <rakesh03101999@gmail.com>
2019-12-21 22:43:26 +0530

    adding the synonym functionality

--- randomWord.js
+++ randomWord.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
-const api_key = '61333f5e04ff167d8eafea290b3ab52e296d8d9df9d64f0f4e37895607658a2990f19f42469b61b9e3bbc16bcf84ee86b11ba7251b007fc1124e28272aa40ddf269201daa753deb68e7090060e67dd74';
-const host = 'https://fourtytwowords.herokuapp.com';
+const API_KEY = '61333f5e04ff167d8eafea290b3ab52e296d8d9df9d64f0f4e37895607658a2990f19f42469b61b9e3bbc16bcf84ee86b11ba7251b007fc1124e28272aa40ddf269201daa753deb68e7090060e67dd74';
+const HOST = 'https://fourtytwowords.herokuapp.com';
 
 async function randomWord () {
     
-    let random_word_url = host + '/words/randomWord?api_key=' + api_key;
-    const response = await fetch(random_word_url);
+    let randomWordUrl = HOST + '/words/randomWord?api_key=' + API_KEY;
+    const response = await fetch(randomWordUrl);
     const json1 = await response.json();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
     return json1.word;
 }
---
+++ wordSyn.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+const API_KEY = '61333f5e04ff167d8eafea290b3ab52e296d8d9df9d64f0f4e37895607658a2990f19f42469b61b9e3bbc16bcf84ee86b11ba7251b007fc1124e28272aa40ddf269201daa753deb68e7090060e67dd74';
+const HOST = 'https://fourtytwowords.herokuapp.com';
+
+async function wordSyn(curWord) {
+
+    
+    let wordSynUrl = HOST + `/word/${curWord}/relatedWords?api_key=` + API_KEY;
+    const response = await fetch(wordSynUrl);
+    let result = await response.json();
+    
+    let arrayWords ;
+    let index = 0;
+    
+    
+   if( (result[0].relationshipType === 'synonym')){ 
+
+        //result index is 0 because synonym can be in 0 or 1 index
+        //it checks in 0 index first if not it will be in index 1
+
+        
+        arrayWords = result[index].words;
+        arrayWords.forEach((def) => {
+          console.log('\n' + def);
+        });
+    } else {
+        index = 1; 
+        arrayWords = result[index].words;
+        arrayWords.forEach((def) => {
+            console.log('\n' + def);
+        });
+    }
+}
+console.log(wordSyn(process.argv.splice(2)));
